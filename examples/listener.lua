f4d7b3ef9ae44af14a61ec3f757f8198b8b98f06
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-07-19 13:01:08 +0300

    Add. Event listener example.

---
+++ examples/listener.lua
@@ -0,0 +1,30 @@
+local uv  = require "lluv"
+local esl = require "lluv.esl"
+
+local fs = { '127.0.0.1', '8021', 'ClueCon' }
+
+local pbx = esl.Connection{ fs[1], fs[2], fs[3],
+  reconnect = 5; no_execute_result = true; no_bgapi = true;
+  subscribe = {
+    'CHANNEL_CREATE'
+  };
+  filter    = {
+    ['Caller-Direction'] = 'inbound',
+  };
+}:open()
+
+pbx:on('esl::event::CHANNEL_CREATE::**', function(self, eventName, event)
+  print(eventName)
+end)
+
+pbx:on('esl::reconnect', function(self, eventName)
+  -- connected to FS
+  print(eventName)
+end)
+
+pbx:on('esl::disconnect', function(self, eventName, err)
+  -- connection lost
+  print(eventName)
+end)
+
+uv.run()
